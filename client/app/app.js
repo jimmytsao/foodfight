49a47114412393273953af39d23dac796fb7e9be
Jimmy Tsao <jimmy.tsao@gmail.com>
2014-10-15 17:52:46 -0500

    (feat) Configured the main app module and added Restangular

--- client/app/app.js
+++ client/app/app.js
@@ -1,19 +1,33 @@
-// Ionic Starter App
+'use strict';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+(function(){
+  
+  angular
+    .module('app', [
+      'ionic',
+      'restangular'])
+    .config(['$urlRouterProvider', configureDefaultRoute])
+    .run(['$ionicPlatform', configureIonicComponents]);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  //library modules
+  window._ = require('lodash');
+  require('restangular');
+
+  function configureDefaultRoute($urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+  }
+
+  function configureIonicComponents($ionicPlatform){
+   $ionicPlatform.ready(function(){
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  }
+})();
+
